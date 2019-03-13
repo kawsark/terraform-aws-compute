19f0b2a2701dadf6eeb1fc4f306836634fca0f3a
kawsark <kawsar@hashicorp.com>
2019-03-13 17:35:44 -0400

    adjusted ami

--- examples/simple-ec2/ami.tf
+++ examples/simple-ec2/ami.tf
@@ -19,22 +19,3 @@ data "aws_ami" "ubuntu" {
 
     owners = ["099720109477"] # Canonical
 }
-
-# AMI finder
-data "aws_ami" "ubuntu_secondary" {
-    most_recent = true
-    provider = "aws.aws_secondary"    
-
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
-    }
-
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-
-    owners = ["099720109477"] # Canonical
-}
-
