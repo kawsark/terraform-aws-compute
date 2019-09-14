822a42979ddba5fb57c88a922b76ad3164fba09d
kawsark <kawsar@hashicorp.com>
2019-09-14 11:30:12 -0400

    updated to terraform 0.12

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,8 @@
 output "public_dns" {
-  value = "${aws_instance.demo_server.*.public_dns}"
+  value = aws_instance.demo_server.*.public_dns
 }
 
 output "id" {
-  value = "${aws_instance.demo_server.0.id}"
+  value = aws_instance.demo_server[0].id
 }
+
